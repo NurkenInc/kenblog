e85d66ab226a4750bea62c4664e4e11429a49ce9
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-07-13 09:32:56 +0600

    project config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com', 'task.com']
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['cloudinary'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/feed',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
